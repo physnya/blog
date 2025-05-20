c5098b06f56166a6c0aaac296cfa0cb70f8a1e55
physnya <676266673@qq.com>
2025-05-20 10:19:33 +0800

    fix(display): KaTeX

--- docs/.vuepress/notes/cosmos.ts
+++ docs/.vuepress/notes/cosmos.ts
@@ -1,28 +1,28 @@
-import { defineNoteConfig } from 'vuepress-theme-plume'
+import { defineNoteConfig } from "vuepress-theme-plume";
 
 export default defineNoteConfig({
-  dir: 'cosmos',
-  link: '/cosmos',
-  sidebar: [
-    {
-      text: '星系与宇宙',
-      collapsed: false,
-      link: '/cosmos/',
-      items: [
-        'lesson-1',
-        'lesson-2',
-        'lesson-3',
-        'lesson-4',
-        'lesson-5',
-        'lesson-6',
-        'lesson-7',
-        'lesson-8',
-        'lesson-9',
-        'lesson-10',
-        'lesson-12',
-        'lesson-13',
-        'lesson-14',
-      ]
-    },
-  ],
-})
+	dir: "cosmos",
+	link: "/cosmos",
+	sidebar: [
+		{
+			text: "星系与宇宙",
+			collapsed: false,
+			link: "/cosmos/",
+			items: [
+				"lesson-1",
+				"lesson-2",
+				"lesson-3",
+				"lesson-4",
+				"lesson-5",
+				"lesson-6",
+				"lesson-7",
+				"lesson-8",
+				"lesson-9",
+				"lesson-10",
+				"lesson-12",
+				"lesson-13",
+				"lesson-14",
+			],
+		},
+	],
+});
