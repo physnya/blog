a61fe705dadc0bbb14ce8ef731d9989838787ab6
physnya <676266673@qq.com>
2025-06-12 02:07:18 +0800

    test: talks

--- scripts/pre-commit.cjs
+++ scripts/pre-commit.cjs
@@ -5,7 +5,7 @@ console.log('Running pre-commit hook...');
 try {
   // 1. 获取最新说说数据
   console.log('Fetching latest talks data...');
-  execSync('node scripts/fetch-talks.js', { stdio: 'inherit' });
+  execSync('node scripts/fetch-talks.cjs', { stdio: 'inherit' });
   
   // 2. 添加更新后的数据文件
   execSync('git add docs/.vuepress/public/talks.json', { stdio: 'inherit' });
